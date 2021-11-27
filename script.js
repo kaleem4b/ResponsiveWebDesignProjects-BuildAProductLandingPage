fa2e78e65206ee068d17211d9de4d1d695cc48ea
Kaleem Bhatti <79290497+kaleem4b@users.noreply.github.com>
2021-11-27 15:16:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,3 @@ window.addEventListener("click", function (e) {
     emailTextBox.style.borderBottom = "2px solid black";
   }
 });
-
-const mainFunc = async function () {
-  const video = fetch(
-    "https://api.pexels.com/videos/videos/7385122"
-  ).then((data) => {
-    console.log(data);
-  });
-  console.log(video);
-};
-
-mainFunc();
-
-
-import { createClient } from '/pexels';
-
-const client = createClient('563492ad6f9170000100000183389731a6c74b32b4538b37414cde0b');
-
-client.video.show({ id: 7385122 }).then(video => {
-    console.log(video);
-});
-
-
